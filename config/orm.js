cc8a75d5fb6ccc7f6745fd69571135e17f6914a6
hansel.tritama <hanselgunawan94@gmail.com>
2017-11-19 02:38:50 -0800

    Save 2

--- config/orm.js
+++ config/orm.js
@@ -6,6 +6,7 @@ var orm = {
   selectAll: function(tableName) {
     var queryString = "SELECT * FROM ??";
     connection.query(queryString, [tableName], function(err, result) {
+      if(err) throw err;
       console.log(result);
     });
   },
@@ -13,12 +14,14 @@ var orm = {
     var queryString = "INSERT INTO ??(??, ??, ??) VALUES(?, FALSE, CURRENT_TIMESTAMP)";
     console.log(queryString);
     connection.query(queryString, [tableName, burgerName, burgerStatus, burgerDate, burgerInputName], function(err, result) {
+      if(err) throw err;
       console.log(result);
     });
   },
   updateOne: function(tableName, tableUpdatedCol, burgerID) {
     var queryString = "UPDATE ?? SET ?? = TRUE WHERE burgerID = ?";
     connection.query(queryString, [tableName, tableUpdatedCol, burgerID], function(err, result) {
+      if(err) throw err;
       console.log(result);
     });
   }
