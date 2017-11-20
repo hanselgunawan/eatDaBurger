2dc80d2dada96b24919a8cf42dea3a3278771b81
hansel.tritama <hanselgunawan94@gmail.com>
2017-11-20 13:02:56 -0800

    Changed cb to callback.

--- config/orm.js
+++ config/orm.js
@@ -3,26 +3,26 @@ var connection = require("./connection.js");
 // Object Relational Mapper (ORM)
 
 var orm = {
-  selectAll: function(tableName, cb) {
+  selectAll: function(tableName, callback) {
     var queryString = "SELECT * FROM ??";
     connection.query(queryString, [tableName], function(err, result) {
       if(err) throw err;
-      cb(result);
+        callback(result);
     });
   },
-  insertOne: function(tableName, burgerName, burgerStatus, burgerDate, burgerInputName, cb) {
+  insertOne: function(tableName, burgerName, burgerStatus, burgerDate, burgerInputName, callback) {
     var queryString = "INSERT INTO ??(??, ??, ??) VALUES(?, FALSE, CURRENT_TIMESTAMP)";
     console.log(queryString);
     connection.query(queryString, [tableName, burgerName, burgerStatus, burgerDate, burgerInputName], function(err, result) {
       if(err) throw err;
-      cb(result);
+        callback(result);
     });
   },
-  updateOne: function(tableName, tableUpdatedCol, burgerID, cb) {
+  updateOne: function(tableName, tableUpdatedCol, burgerID, callback) {
     var queryString = "UPDATE ?? SET ?? = TRUE WHERE burgerID = ?";
     connection.query(queryString, [tableName, tableUpdatedCol, burgerID], function(err, result) {
       if(err) throw err;
-      cb(result);
+        callback(result);
     });
   }
 };
